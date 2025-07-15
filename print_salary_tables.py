3b0d042073894bc3162ca1268f7c1eb7497787d7
Remik202 <olegantr.zdamar@gmail.com>
2025-07-15 17:54:36 +0300

    Сменил регистр переменных

--- print_salary_tables.py
+++ print_salary_tables.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     load_dotenv()
     superjob_key = os.getenv('SUPERJOB_API_KEY')
     
-    SJ_DEVELOPMENT_CATALOGUE_ID = 48,
-    SJ_PAGE_SIZE = 100
+    sj_development_catalogue_id = 48,
+    sj_page_size = 100
 
     languages = ['Python', 'Java', 'C++', 'C#', 'JavaScript', 'Ruby', 'Go', '1C']
 
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     sj_stats = calculate_average_salaries_superjob(
         languages,
         superjob_key,
-        SJ_DEVELOPMENT_CATALOGUE_ID,
-        SJ_PAGE_SIZE
+        sj_development_catalogue_id,
+        sj_page_size
     )
     print_salary_table(sj_stats, 'SuperJob Moscow')

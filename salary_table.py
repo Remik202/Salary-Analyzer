e9adda1d2bfb91bbd9c9aeefab6673492f5b23b1
Remik202 <olegantr.zdamar@gmail.com>
2025-07-15 19:19:29 +0300

    info заменен на vacancies_data

--- salary_table.py
+++ salary_table.py
@@ -1,17 +1,17 @@
 from terminaltables import AsciiTable
 
 
-def print_salary_table(statistics, title):
+def print_salary_table(statistics, vacancies_data, title):
     table_data = [
         ['Язык программирования', 'Найдено вакансий', 'Обработано вакансий', 'Средняя зарплата']
     ]
 
-    for language, info in statistics.items():
+    for language, vacancies_data in statistics.items():
         row = [
             language,
-            info['vacancies_found'],
-            info['vacancies_processed'],
-            info['average_salary']
+            vacancies_data['vacancies_found'],
+            vacancies_data['vacancies_processed'],
+            vacancies_data['average_salary']
         ]
         table_data.append(row)
 
